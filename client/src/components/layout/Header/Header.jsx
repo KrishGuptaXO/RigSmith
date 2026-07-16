687365dfd858e81d99673e88a7f6c7b87f2353e0
KrishGuptaXO <krish.gupta6850@gmail.com>
2026-07-16 16:39:09 +0530

    feat(header): redesign header with search and dynamic greeting

--- client/src/components/layout/Header/Header.jsx
+++ client/src/components/layout/Header/Header.jsx
@@ -5,29 +5,32 @@ import Greetings from '../../common/Greetings';
 
 export default function Header(){
     return (
-        <header className='flex items-center justify-between px-6 py-4 bg-[#0D1117]'>
-            { /* Greetings */}
-            <Greetings user="Krish" />
-            
-            {/* Nav */}
+        <header className='flex flex-col gap-6 bg-[#0D1117] px-8 py-6'>
+
+            {/* Search Row */}
             <div className='flex items-center justify-between'>
 
                 <SearchBar />
 
-                <div className='flex items-center gap-4'>
-                    <Button>
+                <div className='flex items-center gap-3'>
+
+                    <button className="rounded-xl bg-[#161B22] p-3 mr-3 text-zinc-400 transition-all hover:bg-[#1F2937] hover:text-cyan-400 cursor-pointer">
                         <Bell size={20} />
-                    </Button>
-
-                    <Button>
+                    </button>
+                    
+                    <button className="rounded-xl bg-[#161B22] p-3 mr-3 text-zinc-400 transition-all hover:bg-[#1F2937] hover:text-cyan-400 cursor-pointer">
                         <ShoppingCart size={20} />
-                    </Button>
-
-                    <Button>
+                    </button>
+                    
+                    <button className="rounded-xl bg-[#161B22] p-3 mr-3 text-zinc-400 transition-all hover:bg-[#1F2937] hover:text-cyan-400 cursor-pointer">
                         <CircleUser size={20} />
-                    </Button>
+                    </button>
+                
                 </div>
             </div>
+            { /* Greetings */}
+            <Greetings user="Krish" />
+
         </header>
     );
 }
--- client/src/components/layout/Header/SearchBar.jsx
+++ client/src/components/layout/Header/SearchBar.jsx
@@ -2,15 +2,18 @@ import {Search} from "lucide-react"
 
 export default function SearchBar () {
     return(
-        <div className="relative w-full max-w-xl">
+        <div className="relative w-full max-w-3xl">
             
             {/* Icon */}
-            <Search size={18} className="absolute left-4 top-1/2 -translate-y-1/2 text-zinc-500" />
+            <Search 
+                size={18} 
+                className="absolute left-4 top-1/2 -translate-y-1/2 text-zinc-500" 
+            />
 
             {/* Input Field */}
             <input
                 type="text"
-                placeholder="Seach builds, components, orders..."
+                placeholder="Search builds, components, orders..."
                 className="
                     w-full
                     rounded-xl
@@ -18,14 +21,14 @@ export default function SearchBar () {
                     border-zinc-800
                     bg-[#121821]
                     py-3
-                    p1-11
+                    pl-12
                     pr-4
                     text-sm
                     text-white
                     outline-none
                     transition-all
                     duration-300
-                    placeholder: text-zinc-500
+                    placeholder:text-zinc-500
                     focus:border-cyan-400
                     focus:ring-2
                     focus:ring-cyan-400/20"
