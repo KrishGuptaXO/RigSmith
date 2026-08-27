b6ec1269a65aa81ad48972c1ef991af5926d8431
KrishGuptaXO <krish.gupta6850@gmail.com>
2026-08-27 14:15:08 +0530

    Testing backend and DB connections

---
+++ server/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+    res.json({
+        message: "RigSmith API is running.",
+    });
+});
+
+// Connect to MongoDB, then start server
+const startServer = async () => {
+    await connectDB();
+
+    app.listen(PORT, () => {
+        console.log(`Rigsmith server running on port ${PORT}`);
+    });
+};
+
+startServer();
